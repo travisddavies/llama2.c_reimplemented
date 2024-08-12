63f521c275a6570a75b56718220109fdb0749e4b
travisddavies <travisddavies@gmail.com>
2024-08-12 13:37:11 +0800

    completed feed forward and tokenindex struct

--- run.c
+++ run.c
@@ -312,6 +312,48 @@ float* forward(Transformer* transformer, int token, int pos) {
                 }
             }
         }
+
+        // final matmul to get the output of the attention
+        matmul(s->xb2, s->xb, w->wo + l*dim*dim, dim, dim);
+
+        // residual connection back into x
+        for (int i = 0; i < dim; i++) {
+            x[i] += s->xb2[i];
+        }
+
+        // ffn rmsnrom
+        rmsnorm(s->xb, x, w->rms_ffn_weight + l*dim, dim);
+        // Now for FFN in PyTorch we have: self.w2(F.silu(self.w1(x)) * self.w3(x))
+        matmul(s->hb, s->xb, w->w1 + l*dim*hidden_dim, dim, hidden_dim);
+        matmul(s->hb2, s->xb, w->w3 + l*dim*hidden_dim, dim, hidden_dim);
+
+        // SwiGLU non-linearity
+        for (int i = 0; i < hidden_dim; i++) {
+            float val = s->hb[i];
+            // silu(x)=x*sigma(x), where sigma(x) is the logistic sigmoid
+            val *= (1.0f / (1.0f + expf(-val)));
+            // elementwise multiply with w3(x)
+            val *= s->hb2[i];
+            s->hb[i] = val;
+        }
+
+        // residual connection
+        for (int i = 0; i < dim; i++) {
+            x[i] += s->xb[i];
+        }
     }
+
+    // final rmsnrom
+    matmul(s->logits, x, w->wcls, p->dim, p->vocab_size);
+    return s->logits;
 }
+
+
+// ----------------------------------------------------------------------------
+// The Byte Pair Encoding (BPE) Tokeniser that translates strings <-> tokens
+
+typedef struct {
+    char *str;
+    int id;
+} TokenIndex;
 
